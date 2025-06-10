04687a99b732f7409dc1f5e414601e7175faaa2c
Lyka-Getd <97940743+Lyka-Getd@users.noreply.github.com>
2025-06-10 15:01:51 +0800

    Add files via upload

---
+++ CAS.js
@@ -0,0 +1,60 @@
+        /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+        function myFunction() {
+            var x = document.getElementById("myTopnav");
+            if (x.className === "topnav") {
+                x.className += " responsive";
+            } else {
+                x.className = "topnav";
+            }
+        }
+
+        /* Function to display the overlay */
+        function on() {
+            document.getElementById("overlay").style.display = "block";
+        }
+
+        /* Function to hide the overlay */
+        function off() {
+            document.getElementById("overlay").style.display = "none";
+        }
+
+        function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+function openOverlay() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+
+  function closeOverlay() {
+    const overlay = document.getElementById("myOverlay");
+    overlay.classList.add("slide-up");
+    setTimeout(() => {
+      overlay.style.display = "none";
+      overlay.classList.remove("slide-up");
+    }, 500); // Match the duration of the slide-up animation
+  }
+
+var slideIndex = 1;
+showDivs(slideIndex);
+setInterval(function() { plusDivs(1); }, 5000); // Change slide every 5 seconds
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
